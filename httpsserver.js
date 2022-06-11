d806ac19563468ed10eb882e67b91d5eb684edb9
Zeeshan <nzeeshan6@gmail.com>
2022-06-11 17:31:43 +0530

    Pilot Server Test Inititated

--- httpsserver.js
+++ httpsserver.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 const express = require("express");
 const favicon = require('serve-favicon');
 const app = express();
-const bodyParser = require('body-parser');
-const url = require('url');
 
+const url = require('url');
+const path = require('path');
 
 const server = https.createServer({
     key: fs.readFileSync(__dirname + "/cert/key.pem", 'utf-8'),
@@ -14,9 +14,11 @@ const server = https.createServer({
 }, app);
 
 app.use(favicon(__dirname + '/ico.png'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// app.use('/', function(res, req, next){
+//     console.log(res.url);
+//     next();
+// })
+app.use(express.static(path.join(__dirname)));
 
 var io = require('socket.io')(server);
 
@@ -26,27 +28,67 @@ server.listen(3000, function () {
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
     const urlobject = url.parse(req.url, true);
-    // console.log(urlobject);
+    console.log(req.url);
     const filename = "." + urlobject.pathname;
-
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            fs.readFile('./404.html', function (err, html) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
+    if (req.url === '/'){
+        fs.readFile("./index.html", function(err, html){
+            
+            if (err) {
+                fs.readFile('./404.html', function(err, html){
+                    res.writeHead(404, {"Content-Type":"text/html"});
+                    res.end(html);
+                })
+                
+            }
+            else {
+                res.writeHead(200, {"Content-Type":"text/html"});
                 res.end(html);
-            });
-        } else {
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write(data);
-            res.end();
-        }
-    })
-});
+            }
+        })
+        // res.sendFile(__dirname + '/index.html', function(err){
+        //     res.writeHead(200, {"Content-Type":"text/html"});
+        // //         res.end(html);
+        // })
+    }else if (req.url.match("\.css$")){
+        var cssPath = path.join(__dirname + req.url);
+        var fileStream = fs.createReadStream(cssPath, "utf-8");
+        res.writeHead(200, {'Content-Type':'text/html'});
+        fileStream.pipe(res);
+    }else if (req.url.match("\.png$")){
+        var pngPath = path.join(__dirname + req.url);
+        var fileStream = fs.createReadStream(pngPath);
+        res.writeHead(200, {'Content-Type':'image/png'});
+        fileStream.pipe(res);
+    }else if (req.url.match("\.jpg$")){
+        var jpgPath = path.join(__dirname + req.url)
+        var fileStream = fs.createReadStream(jpgPath);
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fileStream.pipe(res);
+    }else if (req.url.match('\.html$')){
+        var filePath = __dirname + req.url;
+        fs.readFile(filePath, function(err, html){
+            if (err) {
+                fs.readFile('./404.html', function(err, html){
+                    res.writeHead(404, {"Content-Type":"text/html"});
+                    res.end(html);
+                })
+                
+            }
+            else {
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.end(html);
+            }
+        })        
+    }else{
+        fs.readFile('./404.html', function(err, html){
+            res.writeHead(404, {"Content-Type":"text/html"});
+            res.end(html);
+        }) 
+    }
 
-app.post("/", function (req, res) {
-    console.log("Inside the post method");
+    
 });
 
 var name = "Zeeshan Naseem";
